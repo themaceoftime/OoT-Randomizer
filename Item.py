370534dbab96b6aa53bd8511bb8f4a4452bb9c49
mracsys <mracsys@gmail.com>
2021-09-06 15:34:11 -0400

    address code review comments

--- Item.py
+++ Item.py
@@ -120,7 +120,7 @@ class Item(object):
         return ((self.type == 'SmallKey' and self.world.settings.shuffle_smallkeys in ['remove','vanilla','dungeon']) or
                 (self.type == 'FortressSmallKey' and self.world.settings.shuffle_fortresskeys in ['vanilla']) or
                 (self.bosskey and self.world.settings.shuffle_bosskeys in ['remove','vanilla','dungeon']) or
-                (self.map or self.compass and self.world.settings.shuffle_mapcompass in ['remove','startwith','vanilla','dungeon']))
+                ((self.map or self.compass) and (self.world.settings.shuffle_mapcompass in ['remove','startwith','vanilla','dungeon'])))
 
     @property
     def majoritem(self):
