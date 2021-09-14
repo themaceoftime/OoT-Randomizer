9a1a10e1f96f69a7bf68d0347d11a437ba7d2ebb
cjohnson57 <C.Johnson.57@outlook.com>
2021-09-13 20:53:42 -0500

    Set DoT open flag while killing collision

--- ASM/src/door_of_time_col_fix.asm
+++ ASM/src/door_of_time_col_fix.asm
@@ -1,6 +1,10 @@
 kill_door_of_time_col:
-    sw      $zero, 0x130(a0) ; update = NULL
-    sw      $zero, 0x134(a0) ; draw = NULL
-    lui     $at, 0x3F80 ; displaced instruction
-    jr      $ra
-    mtc1    $at, $f6 ; displaced instruction
+    sw      zero, 0x130(a0) ; update = NULL
+    sw      zero, 0x134(a0) ; draw = NULL
+    la      at, SAVE_CONTEXT
+    lb      t0, 0x0EDC(at)
+    ori     t0, t0, 0x08 ; "Opened the Door of Time" Flag
+    sb      t0, 0x0EDC(at)
+    lui     at, 0x3F80 ; displaced instruction
+    jr      ra
+    mtc1    at, f6 ; displaced instruction
