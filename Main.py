b634c46823ed63b21306b74e5016dc1043d31d43
cjohnson57 <C.Johnson.57@outlook.com>
2021-09-16 23:01:09 -0500

    Copy settings by value for all but first world

--- Main.py
+++ Main.py
@@ -126,8 +126,9 @@ def generate(settings, window=dummy_window()):
 def build_world_graphs(settings, window=dummy_window()):
     logger = logging.getLogger('')
     worlds = []
-    for i in range(0, settings.world_count):
-        worlds.append(World(i, settings))
+    for i in range(1, settings.world_count):
+        worlds.append(World(i, copy.copy(settings)))
+    worlds.insert(0, World(0, settings))
 
     window.update_status('Creating the Worlds')
     for id, world in enumerate(worlds):
