ffa7f55699ab00df297dcd0ba8c6714b2bdfe3bb
cjohnson57 <C.Johnson.57@outlook.com>
2022-04-21 21:39:07 -0500

    Change message wording

--- ASM/c/model_text.c
+++ ASM/c/model_text.c
@@ -10,14 +10,14 @@ void draw_illegal_model_text(z64_disp_buf_t *db) {
     }
     
     // Setup draw location
-    int str_len = 36;
+    int str_len = 38;
     int total_w = str_len * font_sprite.tile_w;
     int draw_x = Z64_SCREEN_WIDTH / 2 - total_w / 2;
     int draw_y_text = 5;
 
     // Create collected/required string
     char text[str_len + 1];
-    strncpy(text, "Model not legal, irregular skeleton.\0", str_len + 1);
+    strncpy(text, "Race illegal model, irregular skeleton\0", str_len + 1);
 
     // Call setup display list
     gSPDisplayList(db->p++, &setup_db);
