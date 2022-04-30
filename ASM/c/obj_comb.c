d6cde2dcac1d8a944bafc19ee04d53948b9a24f9
RealRob <rrealmuto@gmail.com>
2022-04-30 15:03:36 -0400

    Hack for obj_comb to use custom textures

--- ASM/c/obj_comb.c
+++ ASM/c/obj_comb.c
@@ -1,5 +1,7 @@
 #include "z64.h"
 
+#define GAMEPLAY_FIELD_KEEP_BEEHIVE_TEXTURE (uint8_t*)0x05008900
+
 // Hack beehives to drop a collectible w / an extended flag, based on the grotto param ?
 void obj_comb_drop_collectible(z64_actor_t *actor, int16_t params)
 {
@@ -19,4 +21,14 @@ void obj_comb_drop_collectible(z64_actor_t *actor, int16_t params)
         params |= ((flag & 0x3F) << 8) + (flag & 0xC0);
         z64_Item_DropCollectible2(&z64_game, &actor->pos_2, params);
     }
+}
+
+void ObjComb_Draw_Hack(z64_actor_t* this, z64_game_t* game)
+{
+    uint8_t *texture = GAMEPLAY_FIELD_KEEP_BEEHIVE_TEXTURE;
+    z64_gfx_t *gfx = game->common.gfx;
+    gfx->poly_opa.d -= 2;
+    gDPSetTextureImage(gfx->poly_opa.d, G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, texture);
+    gSPEndDisplayList(gfx->poly_opa.d + 1);
+    gMoveWd(gfx->poly_opa.p++, G_MW_SEGMENT, 9 * sizeof(int), gfx->poly_opa.d);
 }
