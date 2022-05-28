7022c2bae11afdbf22f75962ffdd8496e2dfa742
Fenhl <fenhl@fenhl.net>
2022-05-28 16:25:56 +0000

    Don't include Pre-planted Beans in Randomize Main Rules, fixes #1576

--- SettingsList.py
+++ SettingsList.py
@@ -2806,9 +2806,6 @@ setting_infos = [
         ''',
         default        = False,
         shared         = True,
-        gui_params     = {
-            'randomize_key': 'randomize_settings',
-        },
     ),
     Checkbutton(
         name           = 'chicken_count_random',
