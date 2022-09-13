eb6f1eb6758a1f3c06e2dc73cbb15d57835db8b3
Cuphat <matthew@cuphat.com>
2022-09-12 22:31:18 -0400

    Version Refactor
    Reworks version checks to make it easier for branches to have their own individual version tracking. Version checks are performed using the branch URL specified, and now checks the supplementary version as well.
    
    The version bytes injected into the ROM header at address 0x35 has been expanded to 5 bytes. In addition to the 3 base version bytes, it now has branch identifier for the fourth byte and the supplementary version for the fifth byte.

--- Rom.py
+++ Rom.py
@@ -11,7 +11,7 @@ import copy
 from Utils import is_bundled, subprocess_args, local_path, data_path, default_output_path, get_version_bytes
 from ntype import BigStream, uint32
 from crc import calculate_crc
-from version import __version__
+from version import base_version, branch_identifier, supplementary_version
 
 DMADATA_START = 0x7430
 
@@ -55,8 +55,8 @@ class Rom(BigStream):
         self.original = self.copy()
 
         # Add version number to header.
-        self.write_bytes(0x35, get_version_bytes(__version__))
-        self.force_patch.extend([0x35, 0x36, 0x37])
+        self.write_bytes(0x35, get_version_bytes(base_version, branch_identifier, supplementary_version))
+        self.force_patch.extend([0x35, 0x36, 0x37, 0x38, 0x39])
 
 
     def copy(self):
@@ -133,8 +133,8 @@ class Rom(BigStream):
         self.changed_dma = {}
         self.force_patch = []
         self.last_address = None
-        self.write_bytes(0x35, get_version_bytes(__version__))
-        self.force_patch.extend([0x35, 0x36, 0x37])
+        self.write_bytes(0x35, get_version_bytes(base_version, branch_identifier, supplementary_version))
+        self.force_patch.extend([0x35, 0x36, 0x37, 0x38, 0x39])
 
 
     def sym(self, symbol_name):
